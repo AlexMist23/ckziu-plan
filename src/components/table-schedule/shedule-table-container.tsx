2312c9382b59eb9e15d30a41d21db0d98f2d5f7a
Alexander <aleksander.misterkiewicz@outlook.com>
2024-09-15 20:39:10 +0200

    add card header

--- src/components/table-schedule/shedule-table-container.tsx
+++ src/components/table-schedule/shedule-table-container.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
 import EditableTable from "@/components/table-schedule/schedule-table";
 import { getData } from "@/app/schedule/actions";
-import { Card, CardContent, CardDescription } from "../ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
 
 export default async function SchedulePage() {
   const data = await getData();
@@ -11,10 +11,14 @@ export default async function SchedulePage() {
       <h1 className="text-2xl font-bold mb-4">Plan Zajęć</h1>
       <Suspense fallback={<div>Loading...</div>}>
         <Card>
-          <CardDescription>
-            <CardContent></CardContent>
-          </CardDescription>
-          <EditableTable initialData={data} />
+          <CardHeader>
+            <CardTitle>Plan Zajęć</CardTitle>
+            <CardDescription>Dane z: https://ckziu1.gda.pl/plan-zajec#lo-tryb-zaoczny</CardDescription>
+          </CardHeader>
+          
+          <CardContent>
+            <EditableTable initialData={data} />
+          </CardContent>
         </Card>
       </Suspense>
     </div>
