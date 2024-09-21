cc1b28f7edb1ae58e482051789d5462cb40be40b
Alexander <aleksander.misterkiewicz@outlook.com>
2024-09-21 02:07:56 +0200

    4124

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -121,7 +121,7 @@ export default function PDFLibrary() {
           </SelectContent>
         </Select>
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4 lg:gap-6">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2 md:gap-4 lg:gap-6">
         {filteredBooks.map((book) => (
           <Card
             key={book.pdf}
